fb0bae5236e5aff554bbc5dbeba50a084f4a9721
DESKTOP-7PU8G2V\hanad <hanadabdulqdir@gmail.com>
2018-04-19 12:38:10 +0100

    movie

---
+++ Movie-array/app/src/main/java/project/hanad/com/movie_array/Movie.java
@@ -0,0 +1,42 @@
+package project.hanad.com.movie_array;
+
+/**
+ * Created by hanad on 19/04/2018.
+ */
+
+public class Movie {
+    private String title, genre, year;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String genre, String year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
